eebbacb9a94fa0f381c34e17cd48bb768efacd77
hemrajchoudhary7 <hemraj.rudra@gmail.com>
2024-12-20 23:27:51 +0530

    second

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -5,3 +7,11 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
+
+if __name__ == "__main__":
+    # Fetch host and port from environment variables with defaults
+    host = os.getenv("LISTEN", "0.0.0.0")
+    port = int(os.getenv("PORT", 8000))
+    
+    # Run the FastAPI app with Uvicorn
+    uvicorn.run(app, host=host, port=port)
